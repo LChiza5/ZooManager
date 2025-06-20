2faa44626feaf7b95296f60ae342b5455c9823eb
Chiza <lualvarezva@est.utn.ac.cr>
2025-06-20 14:20:43 -0600

    Clase Animal
    Se crea la clase Animal con sus atributos, contructor y metodos

--- src/RegistroAnimales/Animal.java
+++ src/RegistroAnimales/Animal.java
@@ -15,5 +15,54 @@ public class Animal {
     private String nombre;
     private String especie;
     private LocalDate fechaNacimiento;
-    private Zona zonaActual;
+    private String zonaActual;
+    
+     public static final String ZonaInicial = "Conservación y Rescate";
+     public static final String[] ZONAS = {
+        "Granja", "Contacto", "Alimentación", ZonaInicial,
+        "Túnel Nocturno", "Biodiversidad Local" 
+    };
+
+    public String getIdAnimal() {
+        return idAnimal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getZonaActual() {
+        return zonaActual;
+    }
+
+    public Animal(String idAnimal, String nombre, String especie, LocalDate fechaNacimiento, String zonaActual) {
+        if (idAnimal != null && idAnimal.matches("^[A-Z]-\\d{4}$")) {
+            this.idAnimal = idAnimal;
+        this.nombre = nombre;
+        this.especie = especie;
+        this.fechaNacimiento = fechaNacimiento;
+        this.zonaActual = zonaActual;
+        }
+    }
+    
+    public void moverZona(String nuevaZona) {
+    boolean zonaValida = false;
+    for (String z : ZONAS) {
+        if (z.equalsIgnoreCase(nuevaZona) && !z.equalsIgnoreCase(this.zonaActual)) {
+            zonaValida = true;
+            break;
+        }
+    }
+    if (zonaValida) {
+        this.zonaActual = nuevaZona;
+        }
+    }
 }
