03a4c644b394648b1573e5f7d7c3b18755a1faf5
Chiza <lualvarezva@est.utn.ac.cr>
2025-06-20 13:40:19 -0600

    Paquete RegistroAnimales
    Dentro del paquete RegistroAnimales se creo la clase Animal con sus atributos

---
+++ src/RegistroAnimales/Animal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RegistroAnimales;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Luisk
+ */
+public class Animal {
+    private String idAnimal;
+    private String nombre;
+    private String especie;
+    private LocalDate fechaNacimiento;
+    private Zona zonaActual;
+}
